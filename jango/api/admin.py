e8b0fc29c16fa6036678e287302c4c597a15f223
Ilyas Karim <iksdatoo@gmail.com>
2018-11-29 14:02:23 +0500

    Changes

--- jango/api/admin.py
+++ jango/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post,PostReaction,SavedPost,Profile
+from .models import Post,PostReaction,SavedPost,Profile,Comment
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(PostReaction)
 admin.site.register(SavedPost)
 admin.site.register(Profile)
+admin.site.register(Comment)
